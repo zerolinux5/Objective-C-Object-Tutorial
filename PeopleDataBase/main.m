3c9dc3f88a6496bf45020abea97cf10c02c4905e
Jesus Magana <maganazero5@gmail.com>
2014-06-22 10:26:35 -0700

    Initial Commit

---
+++ PeopleDataBase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeopleDataBase
+//
+//  Created by Jesus Magana on 6/22/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
