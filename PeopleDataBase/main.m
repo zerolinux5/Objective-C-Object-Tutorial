b77daf7d729fb63de57a51a3aeb3e837e6c46fe4
Jesus Magana <maganazero5@gmail.com>
2014-06-22 15:20:37 -0700

    Finished tutorial part

--- PeopleDataBase/main.m
+++ PeopleDataBase/main.m
@@ -12,10 +12,22 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int x = 10;
+        NSString *myString = [NSString stringWithFormat:@"The variable x stores the number %i", x];
+        NSLog(@"%@", myString);
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Please enter a word.");
         
+        // 1
+        char cstring[40];
+        
+        // 2
+        scanf("%s",cstring);
+        
+        // 3
+        NSString *inputString = [NSString stringWithCString:cstring encoding:1];
+        
+        NSLog(@"You entered the word '%@' and it is %li characters long", inputString, [inputString length]);
     }
     return 0;
 }
